5b750481a6bf4399ca844d25ef49c84a14fa6c60
Dmitry Osipovich <dosipovich@hotmail.com>
2022-11-13 11:35:31 +0300

    Задача №64 к 9 семинару решена успешно!

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу,
+которая выведет все натуральные числа в промежутке от N до 1.
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+Console.Write("Введите натуральное число больше 0: ");
+int NatNumber = int.Parse(Console.ReadLine());
+
+void NaturalNumbers(int NatNumber)
+{
+    if (NatNumber < 0)
+    {
+        Console.WriteLine($"Введённое число {NatNumber} - не натуральное число.");
+        return;
+    }
+    if (NatNumber == 0) return;
+    Console.Write("{0}\t", NatNumber);
+    NaturalNumbers(NatNumber - 1);
+}
+
+NaturalNumbers(NatNumber);
